5114bdba1c5c83cb459a85df78d4c241fbfd730e
Mark Lamley <marksl.git@gmail.com>
2013-12-15 10:06:58 -0800

    Generate C# from thrift

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.thriftText = new System.Windows.Forms.TextBox();
+            this.Process = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(42, 111);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // thriftText
+            // 
+            this.thriftText.Location = new System.Drawing.Point(25, 153);
+            this.thriftText.Multiline = true;
+            this.thriftText.Name = "thriftText";
+            this.thriftText.Size = new System.Drawing.Size(486, 241);
+            this.thriftText.TabIndex = 1;
+            this.thriftText.Text = resources.GetString("thriftText.Text");
+            // 
+            // Process
+            // 
+            this.Process.Location = new System.Drawing.Point(25, 400);
+            this.Process.Name = "Process";
+            this.Process.Size = new System.Drawing.Size(486, 51);
+            this.Process.TabIndex = 2;
+            this.Process.Text = "Process";
+            this.Process.UseVisualStyleBackColor = true;
+            this.Process.Click += new System.EventHandler(this.Process_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(25, 469);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 2);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 657);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Process);
+            this.Controls.Add(this.thriftText);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox thriftText;
+        private System.Windows.Forms.Button Process;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Tasks;
+using Microsoft.Build.Utilities;
+
+namespace TestApp
+{
+ 
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<Item> items = new List<Item>
+                                   {
+                                       new Item("https://lb-sm-cpqa2-van.dev-globalrelay.net:7443/core/message.thrift", 1), 
+                                       new Item("https://lb-sm-cpqa1-van.dev-globalrelay.net:7443/core/message.thrift", 2),
+                                       new Item("https://lb-sm-smsnap1-van.dev-globalrelay.net:7443/core/message.thrift", 3)
+                                   };
+            this.comboBox1.Width = 300;
+            this.comboBox1.DataSource = items;
+            this.comboBox1.DisplayMember = "Name";
+            this.comboBox1.ValueMember = "Id";}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Process_Click(object sender, EventArgs e)
+        {
+            // 1. Save textbox to .thrift file
+            //thriftText.Text
+
+
+            CreateThriftFile("temp.thrift");
+
+            // 2. Run code gen on .thrift file
+
+            var outputFolder = Path.Combine(Directory.GetCurrentDirectory(), "gen-csharp");
+            if (Directory.Exists(outputFolder))
+            {
+                try
+                {
+                    Directory.Delete(outputFolder, true);
+                }
+                catch
+                {
+                    // Delete failed. Overwrite the files and hope for the best.
+                }
+            }
+
+            var process = new System.Diagnostics.Process();
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+                                {
+                                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                                    FileName = "cmd.exe",
+                                    Arguments = "/C thrift-0.9.0.exe --gen csharp " + "temp.thrift"
+                                };
+            process.StartInfo = startInfo;
+            process.Start();
+
+
+
+            // 3. Compile thrift file.
+
+            // Create a new Engine object.
+            FileLogger logger = new FileLogger();
+            // Set the logfile parameter to indicate the log destination
+            logger.Parameters = @"logfile=C:\git\thrifter\build.log";
+
+            string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project ToolsVersion=""4.0"" DefaultTargets=""Build"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+<ItemGroup>
+  <Reference Include=""Thrift"">
+      <HintPath>{0}</HintPath>
+      <Private>False</Private>
+</Reference>
+</ItemGroup>
+
+
+    <Import Project=""$(MSBuildToolsPath)\Microsoft.CSharp.targets"" />
+
+  
+</Project>
+";
+
+  //          <PropertyGroup>
+  //  <PrepareForRunDependsOn>$(PrepareForRunDependsOn);MyCopyFilesToOutputDirectory</PrepareForRunDependsOn>
+  //</PropertyGroup>
+
+  //<Target Name=""MyCopyFilesToOutputDirectory"">
+  //  <Copy SourceFiles=""@(None)"" DestinationFolder=""$(OutDir)"" />
+  //</Target>
+
+            xml = string.Format(xml, Path.Combine(Directory.GetCurrentDirectory(), "Thrift.dll"));
+
+            byte[] data = Encoding.Default.GetBytes(xml);
+
+
+            XmlReader reader = new XmlTextReader(new MemoryStream(data));
+
+            // Create a new Project object.
+            var project = new Project(reader);
+            project.FullPath = Path.Combine(Directory.GetCurrentDirectory(), ".\\foo\\");
+            
+            //var propertyGroup = project.Properties
+            var properties = new Dictionary<string, string>
+                                 {
+                                     {"Configuration", "Debug"},
+                                     {"Platform","AnyCPU"},
+                                     {"ProjectGuid", Guid.NewGuid().ToString()},
+                                     {"ProjectVersion", "8.0.30703"},
+                                     {"SchemaVersion", "2.0"},
+                                     {"AssemblyName", "TempThriftGen"},
+                                     {"TargetFrameworkVersion", "v4.0"},
+                                     {"OutputType", "Library"},
+                                     {"FileAlignment", "512"},
+                                     {"DebugSymbols", "true"},
+                                     {"DebugType", "full"},
+                                     {"Optimize", "false"},
+                                     {"OutputPath", ".\\compiled\\"},
+                                     {"DefineConstants", "DEBUG;TRACE"}
+                                 };
+            foreach (var prop in properties)
+            {
+                project.SetProperty(prop.Key, prop.Value);
+            }
+
+            foreach (string file in Directory.GetFiles(outputFolder, "*.cs"))
+            {
+                int index = file.LastIndexOf("\\gen-csharp", StringComparison.Ordinal);
+                string classFileName = file.Substring(index + 1, file.Length - index -1);
+
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), classFileName);
+                project.AddItem("Compile", fullPath);
+            }
+
+            project.AddItem("Reference", "System.Core");
+
+            project.Build("Build", new List<ILogger>{logger});
+
+
+            // Load the project with the following XML, which contains 
+            // two ItemGroups.
+
+
+            // TODO: Make this recursive
+
+            //csc.BuildEngine = new Microsoft.Build.Engine();
+        }
+
+        private void CreateThriftFile(string thriftFileName)
+        {
+            var fileName = Path.Combine(Directory.GetCurrentDirectory(), thriftFileName);
+
+            File.Delete(fileName);
+
+            using (FileStream fs = File.Create(fileName))
+            {
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(thriftText.Text);
+                }
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Item(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+
+    public class MyTask : Task
+    {
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+
+}
